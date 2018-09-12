c4162cc5fcd216a4902c70e8cd8f8b6151369f04
iamkentleom <ssd@SSDs-iMac.local>
2018-09-12 17:35:46 +0800

    Working animation

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 	$(".box2").hide();
 	$("#goalEntered").click(function(){
-		// $(".box2").show();
+		
 		// $(".box2").css("width","80vmin");
-		$(".b1").hide();
-		$(".box").css({
-			"width": "0",
-			"height": "0",
-		});
-		// $(".box").hide();
+		// $(".b1").css("visibility", "hidden");
+		$(".box").css("left", "0");
+		$(".box2").show();
+		$(".box2").css("left", "50%");
+		$(".box").hide();
 		console.log("try");
 	});
 });
