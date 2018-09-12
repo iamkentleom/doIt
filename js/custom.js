626e97baac381496c46d2894468124011af5d7c9
iamkentleom <ssd@SSDs-iMac.local>
2018-09-12 17:43:03 +0800

    Final Animation

--- js/custom.js
+++ js/custom.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
 		
 		// $(".box2").css("width","80vmin");
 		// $(".b1").css("visibility", "hidden");
-		$(".box").css("left", "0");
-		$(".box2").show();
-		$(".box2").css("left", "50%");
-		$(".box").hide();
+		// $(".box").css("left", "0");
+		$(".box").animate({left: '-50vw'});
+		// $(".box2").show(250);
+		// $(".box2").css("left", "50%");
+		// $(".box").hide(250);
 		console.log("try");
 	});
 });
