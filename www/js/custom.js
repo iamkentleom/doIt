ff05fda78f6210b5cf94b0a2d52ecba2aa3cd6e3
iamkentleom <ssd@SSDs-iMac.local>
2018-09-24 10:19:37 +0800

    working animations

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 	$(".box2").hide();
 	$("#goalEntered").click(function(){
-		
-		// $(".box2").css("width","80vmin");
-		// $(".b1").css("visibility", "hidden");
-		// $(".box").css("left", "0");
-		$(".box").animate({left: '-50vw'},1000, "linear");
-		$(".goal").css("background-image", "url('../img/deadline.jpg')");
-		// $(".box2").show(250);
-		// $(".box2").css("left", "50%");
-		// $(".box").hide(250);
-		console.log("try");
+		$(".box").animate({left: '-50vw'},500);
+		$(".goal").css("background-image", "url('img/deadline.jpg')");
+		$(".box2").show();
+		$(".box2").animate({left: "50%"}, 500);
+		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
+	});
+	$("#backGoal").click(function(){
+		$(".layer").css("background-color","rgba(0, 153, 51, 0.5)");
+		$(".box2").animate({left: "150%"}, 500);
+		$(".box2").hide();
+		$(".goal").css("background-image", "url('img/goal.jpeg')");
+		$(".box").animate({left: '50vw'},500);
 	});
 
 });
