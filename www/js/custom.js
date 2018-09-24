98b5a5e03e117b9e87658edc3df41134676b9951
iamkentleom <ssd@SSDs-iMac.local>
2018-09-24 10:45:51 +0800

    payment ui

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
-	$(".box2").hide();
+	$(".b2,.b3").hide();
 	$("#goalEntered").click(function(){
-		$(".box").animate({left: '-50vw'},500);
+		$(".b1").animate({left: '-50vw'},500);
 		$(".goal").css("background-image", "url('img/deadline.jpg')");
-		$(".box2").show();
-		$(".box2").animate({left: "50%"}, 500);
+		$(".b2").show();
+		$(".b2").animate({left: "50%"}, 500);
 		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
 	});
 	$("#backGoal").click(function(){
 		$(".layer").css("background-color","rgba(0, 153, 51, 0.5)");
-		$(".box2").animate({left: "150%"}, 500);
-		$(".box2").hide();
+		$(".b2").animate({left: "150%"}, 500);
+		$(".b2").hide(250);
 		$(".goal").css("background-image", "url('img/goal.jpeg')");
-		$(".box").animate({left: '50vw'},500);
+		$(".b1").animate({left: '50vw'},500);
+	});
+	$("#dateEntered").click(function(){
+		$(".b2").animate({left: '-50vw'},500);
+		$(".goal").css("background-image", "url('img/payment.jpg')");
+		$(".b3").show();
+		$(".b3").animate({left: "50%"}, 500);
+		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
+	});
+	$("#backDate").click(function(){
+		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
+		$(".b3").animate({left: "150%"}, 500);
+		$(".b3").hide(250);
+		$(".goal").css("background-image", "url('img/deadline.jpg')");
+		$(".b2").animate({left: '50vw'},500);
 	});
 
 });
