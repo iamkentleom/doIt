475ae7cf8f588720abd1e84678619d94839804b7
iamkentleom <bmc@GERRAN-CSU-3.local>
2018-09-26 15:29:18 +0800

    error in btn

--- www/js/custom.js
+++ www/js/custom.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	$(".b2,.b3").hide();
 	$("#goalEntered").click(function(){
 		$(".b1").animate({left: '-50vw'},500);
-		$(".goal").css("background-image", "url('img/deadline.jpg')");
+		$(".goal").css("background-image", "url('img/deadline.jpeg')");
 		$(".b2").show();
 		$(".b2").animate({left: "50%"}, 500);
 		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
 	});
 	$("#backGoal").click(function(){
-		$(".layer").css("background-color","rgba(0, 153, 51, 0.5)");
+		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
 		$(".b2").animate({left: "150%"}, 500);
 		$(".b2").hide(250);
 		$(".goal").css("background-image", "url('img/goal.jpeg')");
@@ -16,7 +16,7 @@ $(document).ready(function(){
 	});
 	$("#dateEntered").click(function(){
 		$(".b2").animate({left: '-50vw'},500);
-		$(".goal").css("background-image", "url('img/payment.jpg')");
+		$(".goal").css("background-image", "url('img/supervisor.jpeg')");
 		$(".b3").show();
 		$(".b3").animate({left: "50%"}, 500);
 		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
@@ -25,7 +25,7 @@ $(document).ready(function(){
 		$(".layer").css("background-color","rgba(0, 0, 0, 0.5)");
 		$(".b3").animate({left: "150%"}, 500);
 		$(".b3").hide(250);
-		$(".goal").css("background-image", "url('img/deadline.jpg')");
+		$(".goal").css("background-image", "url('img/deadline.jpeg')");
 		$(".b2").animate({left: '50vw'},500);
 	});
 
